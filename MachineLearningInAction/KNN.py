1b2daf25e458a91bf0efb90644c0796931eb4b34
@lonley <zczysz@gmail.com>
2014-08-22 18:45:57 +0800

    KNN OK

---
+++ MachineLearningInAction/KNN.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+# uses python2.7.6
+# @Date    : 2014-08-22 15:59:58
+# @Author  : lonely
+# @Link    : http://blog.csdn.net/zc937533415
+# @Version : $Id$
+
+
+"""
+k-Nearest Neighbor
+"""
+
+
+from numpy import *
+import operator
+
+
+def createDataSet():
+    group = array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
+    labels = ['A', 'A', 'B', 'B']
+    return group, labels
+
+
+def classify0(inX, dataSet, labels, k):
+    dataSetSize = dataSet.shape[0]
+    diffMat = tile(inX, (dataSetSize, 1)) - dataSet
+    sqDiffMat = diffMat ** 2
+    sqDistances = sqDiffMat.sum(axis=1)
+    distances = sqDistances ** 0.5
+    sortedDistIndicies = distances.argsort()
+    classCount = {}
+    for i in range(k):
+        votelabel = labels[sortedDistIndicies[i]]
+        classCount[votelabel] = classCount.get(votelabel, 0) + 1
+    sortedClassCount = sorted(classCount.iteritems(),
+                              key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
+
+if __name__ == '__main__':
+    group, labels = createDataSet()
+    print classify0([0, 0], group, labels, 3)
+# This is the end
