c042b806d9356b3bdee248ba524c6896075d217c
@lonley <zczysz@gmail.com>
2014-08-22 10:08:33 +0800

    init

---
+++ wxPython/nineth-chapter/MessageBox.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# uses python2.7.6
+# @Date    : 2014-08-22 10:07:04
+# @Author  : lonely
+# @Link    : http://blog.csdn.net/zc937533415
+# @Version : $Id$
+
+
+"""
+消息框
+"""
+
+# This is the end
