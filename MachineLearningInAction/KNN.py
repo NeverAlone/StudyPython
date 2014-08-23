2c623da01d00daff864dbff0323e9bc08bc38d9a
@lonley <zczysz@gmail.com>
2014-08-23 14:43:32 +0800

    分类器完成

--- MachineLearningInAction/KNN.py
+++ MachineLearningInAction/KNN.py
@@ -23,18 +23,30 @@ def createDataSet():
 
 
 def classify0(inX, dataSet, labels, k):
+    print 'dataSet:', dataSet
     dataSetSize = dataSet.shape[0]
+    print u'重复:', tile(inX, (dataSetSize, 1))
+    print 'dataSetSize:', dataSetSize
     diffMat = tile(inX, (dataSetSize, 1)) - dataSet
+    print 'diffMat:', diffMat
     sqDiffMat = diffMat ** 2
+    print 'sqDiffMat:', sqDiffMat
     sqDistances = sqDiffMat.sum(axis=1)
+    print 'sqDistances:', sqDistances
     distances = sqDistances ** 0.5
     sortedDistIndicies = distances.argsort()
+    print 'sortedDistIndicies:', sortedDistIndicies
     classCount = {}
     for i in range(k):
         votelabel = labels[sortedDistIndicies[i]]
+        print 'votelabel:', votelabel, 'classCount.get(votelabel, 0):',
+        classCount.get(votelabel, 0)
         classCount[votelabel] = classCount.get(votelabel, 0) + 1
+        print 'classCount:', classCount
     sortedClassCount = sorted(classCount.iteritems(),
                               key=operator.itemgetter(1), reverse=True)
+    print 'classCount.iteritems()', classCount.iteritems()
+    print 'sortedClassCount', sortedClassCount
     return sortedClassCount[0][0]
 
 if __name__ == '__main__':
